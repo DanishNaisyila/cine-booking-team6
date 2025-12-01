78d94e4c307fc3ecdea5be01e08176baf44838ec
viinafai <vinafaiza25@gmail.com>
2025-12-01 21:00:41 +0700

    profile vina 3

---
+++ lib/pages/profile_page_vina.dart
@@ -0,0 +1,66 @@
+import 'package:cine_booking_team6/controllers/auth_controller_dian.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../controllers/auth_controller_dian.dart';
+//import '../history/history_page_nadhif.dart';
+import '../pages/login_page_azka.dart';
+
+class ProfilePageVina extends StatelessWidget {
+  const ProfilePageVina({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthControllerDian>(context);
+    final user = auth.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Akun Anda",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+
+            Text("Email : ${user?.email}"),
+            const SizedBox(height: 10),
+            Text("UID   : ${user?.uid}"),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //       builder: (_) => const HistoryPageNadhif()),
+                // );
+              },
+              child: const Text("Lihat Riwayat Booking"),
+            ),
+
+            const Spacer(),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () async {
+                await auth.logoutAzka();
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => const LoginPageAzka()),
+                );
+              },
+              child: const Text("Logout"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
